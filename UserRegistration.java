3be4521c99197d9092f267b219d1a7a34ee48ff1
Roshan Gupta <roshangupta@Roshans-MacBook-Air.local>
2021-01-05 17:15:22 +0530

    uc10:Added mood analyser user details

--- UserRegistration.java
+++ UserRegistration.java
@@ -1,80 +1,34 @@
-import java.util.Scanner;
+package userRegistration;
 
 public class UserRegistration {
-    Scanner sc = new Scanner(System.in);
+    public boolean checkFName(String fName) {
+        return (fName.matches("[A-Z][a-z]{3,}"));
 
-    public static void main(String[] args) {
-        UserRegistration obj = new UserRegistration();
-        if (obj.checkName()  == true) {
-            System.out.println("Entered name is in correct format");
-        } else {
-            System.out.println("PLEASE Enter  a valid name");
-        }
-        UserRegistration obj1 = new UserRegistration();
-        if(obj1.checkEmail() == true) {
-            System.out.println("Entered email is in correct format");
-        } else {
-            System.out.println("Enter valid email");
-        }
-
-        UserRegistration obj2 = new UserRegistration();
-        if(obj2.checkPhoneNumber() == true) {
-            System.out.println("Entered phone number is in correct format");
-        } else {
-            System.out.println("Enter valid phone number");
-        }
-
-        UserRegistration obj3 = new UserRegistration();
-        if(obj3.checkPassword() == true) {
-            System.out.println("Entered password is in correct format");
-        } else {
-            System.out.println("Enter valid phone number");
-        }
     }
 
-    public boolean checkName() {
-        System.out.println("Enter the  first name");
-        String firstName = sc.next();
-        System.out.println("Enter the last name");
-        String lastName = sc.next();
-
-        if (firstName.matches("[A-Z][a-z]{3,}") && (lastName.matches("[A-Z][a-z]{3,}") == true) == true) {
-            return true;
-        } else {
-            return false;
-        }
+    public boolean Lname(String Lname) {
+        return (Lname.matches("[A-Z][a-z]{3,}"));
     }
 
-    public boolean checkEmail() {
-        System.out.println("Enter the email");
-        String email = sc.next();
-        if(email.matches( "^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9A-Za-z]+[.][a-zA-Z]{2,4}([.][a-zA-Z]{2,4})*$" ) == true){
-            return true;
+    public boolean checkEmail(String emailID) {
+        return (emailID.matches("^[0-9a-zA-Z]+([._+-][0-9a-zA-Z]+)*[@][0-9A-Za-z]+([.][a-zA-Z]{2,4})*$"));
+    }
+
+    public boolean checkPhoneNum(String phoneNum) {
+        return (phoneNum.matches("^[0-9]{1,2}[ ][0-9]{10}$"));
+    }
+
+    public boolean checkPassword(String password) {
+        return  (password.matches("^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[*.!@#$%^&(){}:'<>,.>/~`_+=|].).{8,}$"));
+    }
+
+    public String moodAnalyzer(String fName, String Lname, String phoneNum, String emailID, String password) {
+        if(checkFName(fName) == true && Lname(Lname) == true && checkEmail(emailID) == true && checkPhoneNum(phoneNum) == true && checkPassword(password) == true){
+            return "HAPPY";
         }
         else {
-            return false;
+            return "SAD";
         }
     }
 
-    public boolean checkPhoneNumber() {
-        System.out.println("Enter the Phone Number");
-        String phoneNumber = sc.next();
-        if(phoneNumber.matches( "^[0-9]{1,2}[ ][0-9]{10}$") == true ){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
-
-    public boolean checkPassword() {
-        System.out.println("Please Enter the Password");
-        String phoneNumber = sc.next();
-        if(phoneNumber.matches( "^(?=.*[A-Z])(?=.*[a-z])(?=.*[0-9])(?=.*[*.!@#$%^&(){}:'<>,.>/~`_+=|].).{8,}$" ) == true ){
-            return true;
-        }
-        else {
-            return false;
-        }
-    }
 }
